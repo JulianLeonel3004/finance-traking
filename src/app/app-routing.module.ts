968ce0103029dce16bd2cbf2f7ed32e893cbc0ee
julianlopez <julian.leonel3004@gmail.com>
2021-11-06 04:39:15 -0300

    start input money

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'list-expenses',
     pathMatch: 'full'
   },
   {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -9,8 +10,47 @@ import { NavController } from '@ionic/angular';
 export class HomePage {
 
   total:number = 15093;
+  inputForm:FormGroup;
+  input:number=0;
+  expenses:number=0;
 
-  constructor(private navController:NavController) {}
+  constructor(private navController:NavController, 
+    private storage:Storage,
+    private formBuilder:FormBuilder) {
+
+      this.inputForm = formBuilder.group({
+        input: new FormControl("", Validators.compose([
+          Validators.required
+        ]))
+      })  
+  }
+
+  ngOnInit() {
+   /* this.storage.create();
+    this.storage.get("input")
+    .then(value => {
+      if(value != null){
+        this.input = value;
+      }
+    })
+    .catch(()=>{
+      alert("ocurrio un error")
+    })
+
+
+
+    this.storage.get("expenseList")
+    .then(expList => {
+        if(expList){
+          expList.forEach(item => {
+            item.products.forEach(prod => {
+              this.expenses += prod.price
+            });
+          });
+        }
+    })*/
+  }
+  
 
   goToExpenses() {
     this.navController.navigateForward('/list-expenses');
